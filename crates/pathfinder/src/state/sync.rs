4e2df520a04c04a5e6c4463672128a1aaee3fe2b
Krzysztof Lis <klis33@gmail.com>
2023-06-22 13:30:44 +0200

    feat(sync): use those extensions in sync

--- crates/pathfinder/src/state/sync.rs
+++ crates/pathfinder/src/state/sync.rs
@@ -26,7 +26,7 @@ use starknet_gateway_client::GatewayApi;
 use starknet_gateway_types::reply::{PendingBlock, PendingStateUpdate};
 use starknet_gateway_types::{
     pending::PendingData,
-    reply::{state_update::DeployedContract, Block, MaybePendingBlock, StateUpdate},
+    reply::{state_update::DeployedContract, Block, StateUpdate},
 };
 
 use std::{collections::HashMap, future::Future};
@@ -548,19 +548,13 @@ async fn update_sync_status_latest(
     starting_block_num: BlockNumber,
     poll_interval: Duration,
 ) -> anyhow::Result<()> {
-    use pathfinder_common::BlockId;
-
     let starting = NumberedBlock::from((starting_block_hash, starting_block_num));
 
     loop {
-        match sequencer.block(BlockId::Latest).await {
-            Ok(MaybePendingBlock::Block(block)) => {
-                let latest = {
-                    let latest_hash = block.block_hash;
-                    let latest_num = block.block_number;
-                    NumberedBlock::from((latest_hash, latest_num))
-                };
-                // Update the sync status.
+        match sequencer.head().await {
+            Ok((block_number, block_hash)) => {
+                let latest = NumberedBlock::from((block_hash, block_number));
+
                 match &mut *state.status.write().await {
                     sync_status @ Syncing::False(_) => {
                         *sync_status = Syncing::Status(syncing::Status {
@@ -589,9 +583,6 @@ async fn update_sync_status_latest(
                         }
                     }
                 }
-            }
-            Ok(MaybePendingBlock::Pending(_)) => {
-                tracing::error!("Latest block returned 'pending'");
             }
             Err(e) => {
                 tracing::error!(error=%e, "Failed to fetch latest block");
